76ee0adfb16d0454ff6733940109a75aa77208da
furkankykc <furkanfbr@gmail.com>
2019-03-05 14:34:54 +0300

    configure allowed hosts

--- instagramRegisterBot/settings.py
+++ instagramRegisterBot/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'pvh^ea66wy_5$!j-!5i7-#rgphxi1-htq35j+=8i_vnif)w#rl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['furkankykc.me:8000']
 
 # Application definition
 
